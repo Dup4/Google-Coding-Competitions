23aa8d83365e1e3f5e1ae2098adcba657c8962f4
Dup4 <38343778+Dup4@users.noreply.github.com>
2020-09-09 14:37:47 +0800

    up

---
+++ KickStart/2020/RoundE/b/b.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n" 
+#define fi first
+#define se second
+#define SZ(x) ((int)(x).size())
+#define mkp make_pair
+#define all(x) (x).begin(), (x).end()
+using db = double;
+using ll = long long;
+using ull = unsigned long long; 
+using pII = pair <int, int>;
+using pLL = pair <ll, ll>;
+constexpr int mod = 1e9 + 7;
+template <class T1, class T2> inline void chadd(T1 &x, T2 y, int Mod = mod) { x += y; while (x >= Mod) x -= Mod; while (x < 0) x += Mod; } 
+template <class T1, class T2> inline void chmax(T1 &x, T2 y) { if (x < y) x = y; }
+template <class T1, class T2> inline void chmin(T1 &x, T2 y) { if (x > y) x = y; }
+inline int nextInt() { int x; cin >> x; return x; }
+void rd() {}
+template <class T, class... Ts> void rd(T& arg, Ts&... args) { cin >> arg; rd(args...); }
+#define dbg(x...) do { cout << "\033[32;1m" << #x << " -> "; err(x); } while (0) 
+void err() { cout << "\033[39;0m" << endl; } 
+template <class T, class... Ts> void err(const T& arg, const Ts&... args) { cout << arg << ' '; err(args...); }
+template <template<typename...> class T, typename t, typename... A> 
+void err(const T <t> &arg, const A&... args) { for (auto &v : arg) cout << v << ' '; err(args...); }
+void ptt() { cout << endl; }
+template <class T, class... Ts> void ptt(const T& arg, const Ts&... args) { cout << ' ' << arg; ptt(args...); }
+template <class T, class... Ts> void pt(const T& arg, const Ts&... args) { cout << arg; ptt(args...); }
+void pt() {}
+template <template<typename...> class T, typename t, typename... A>
+void pt(const T <t> &arg, const A&... args) { for (int i = 0, sze = arg.size(); i < sze; ++i) cout << arg[i] << " \n"[i == sze - 1]; pt(args...); }
+inline ll qpow(ll base, ll n) { assert(n >= 0); ll res = 1; while (n) { if (n & 1) res = res * base % mod; base = base * base % mod; n >>= 1; } return res; }
+//head
+constexpr int N = 1e5 + 10; 
+int n, a, b, c, cnt[2][N];
+
+bool check(vector <int> vec) {
+	for (int i = 0; i <= n; ++i) cnt[0][i] = cnt[1][i] = 0;
+	int Max = 0;
+	int _a = 0, _b = 0, _c = 0;
+	int i = 0;
+	for (auto &it : vec) {
+		chmax(Max, it);
+		if (it == Max) ++cnt[0][i], ++_a;
+		++i;
+	}
+	Max = 0;
+	for (int i = n - 1; i >= 0; --i) {
+		chmax(Max, vec[i]);
+		if (vec[i] == Max) ++cnt[1][i], ++_b;
+	}
+	for (int i = 0; i <= n; ++i) {
+		_c += (cnt[0][i] && cnt[1][i]);
+	}
+//	pt(_a, _b, _c);
+	return _a == a && _b == b && _c == c;
+}
+
+void NO() {
+	pt("IMPOSSIBLE");
+}
+
+void run() {
+	rd(n, a, b, c);
+	if (n == 1) {
+		return pt(1);
+	} else if (n == 2) {
+		if (a == 1 && b == 2) {
+			pt(2, 1);
+		} else if (a == 2 && b == 1) {
+			pt(1, 2);
+		} else if (a == 2 && b == 2 && c == 2) {
+			pt(2, 2);
+		} else {
+			NO();
+		}
+		return;
+	}
+	vector <int> res;
+	if (a == b && c == a) {
+		if (c == 1) return NO();
+		for (int i = 1; i < a; ++i) res.push_back(n);
+		for (int i = 1; i <= n - a; ++i) res.push_back(1);
+		res.push_back(n);
+		return pt(res);
+	}
+	int _a = a - c, _b = b - c;
+	int remind = n - _a - _b - c;
+	if (remind < 0) return NO();
+	for (int i = 1; i <= _a; ++i) res.push_back(n - 1);
+	if (_a) for (int i = 1; i <= remind; ++i) res.push_back(1);
+	for (int i = 1; i <= c; ++i) res.push_back(n);
+	if (!_a && _b) for (int i = 1; i <= remind; ++i) res.push_back(1);
+	for (int i = 1; i <= _b; ++i) res.push_back(n - 1);
+	pt(res);
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	cout << fixed << setprecision(20);
+	int _T = nextInt();
+//	while (_T--) run(); 
+    for (int kase = 1; kase <= _T; ++kase) {
+        cout << "Case #" << kase << ": ";
+        run();
+    }
+//	while (cin >> n) run();
+//	run();
+	return 0;
+}
